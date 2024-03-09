2fcc341699a5e23ab74d237d2d94183d3c1ddb46
ashtishad <ashefgo@gmail.com>
2024-03-10 02:47:57 +0600

    Made parked_at and unparked_at as TIMESTAMPTZ. Time will be maintained in UTC.

--- db/init/01.create-database.sql
+++ db/init/01.create-database.sql
@@ -2,15 +2,6 @@ CREATE DATABASE gopark;
 \c gopark
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS users
-(
-    id      SERIAL PRIMARY KEY,
-    user_id UUID DEFAULT uuid_generate_v4(),
-    name    VARCHAR(255) NOT NULL,
-    role    VARCHAR(255) NOT NULL
-        CHECK (role IN ('general', 'manager'))
-);
-
 CREATE TABLE IF NOT EXISTS parking_lots
 (
     id             SERIAL PRIMARY KEY,
@@ -34,7 +25,6 @@ CREATE TABLE IF NOT EXISTS vehicles
     vehicle_id          UUID DEFAULT uuid_generate_v4(),
     registration_number VARCHAR(255) NOT NULL,
     slot_id             INTEGER      NOT NULL REFERENCES slots (id),
-    parked_at           TIMESTAMP    NOT NULL,
-    unparked_at         TIMESTAMP,
-    user_id             INTEGER      NOT NULL REFERENCES users (id)
+    parked_at           TIMESTAMPTZ    NOT NULL,
+    unparked_at         TIMESTAMPTZ
 );
