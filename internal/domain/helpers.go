980ce937973cef8acb261ad5ece78c793409e1d1
ashtishad <ashefgo@gmail.com>
2024-03-10 05:28:04 +0600

    Created two quick helpers getParkingLotIdByUUID and getSlotIDByUUID. * Purpose is to call it from all repository methods when needed.

---
+++ internal/domain/helpers.go
@@ -0,0 +1,39 @@
+package domain
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log/slog"
+
+	"github.com/ashtishad/gopark/internal/common"
+	"github.com/google/uuid"
+)
+
+// getParkingLotIdByUUID retrieves the internal integer ID of a parking lot given its UUID.
+func getParkingLotIDByUUID(ctx context.Context, db *sql.DB, l *slog.Logger, parkingLotUUID uuid.UUID) (int, common.AppError) {
+	var parkingLotID int
+	err := db.QueryRowContext(ctx, `SELECT id FROM parking_lots WHERE uuid = $1`, parkingLotUUID).Scan(&parkingLotID)
+	if errors.Is(err, sql.ErrNoRows) {
+		l.Error("parking lot not found", "err", err)
+		return 0, common.NewNotFoundError(common.ErrUnexpectedDatabase)
+	} else if err != nil {
+		l.Error("error fetching parking lot ID by uuid", "err", err)
+		return 0, common.NewInternalServerError(common.ErrUnexpectedDatabase, err)
+	}
+	return parkingLotID, nil
+}
+
+// getSlotIDByUUID retrieves the internal integer ID of a slot given its UUID.
+func getSlotIDByUUID(ctx context.Context, db *sql.DB, l *slog.Logger, slotUUID uuid.UUID) (int, common.AppError) {
+	var slotID int
+	err := db.QueryRowContext(ctx, `SELECT id FROM slots WHERE slot_id = $1`, slotUUID).Scan(&slotID)
+	if errors.Is(err, sql.ErrNoRows) {
+		l.Error("slot not found", "err", err)
+		return 0, common.NewNotFoundError(common.ErrUnexpectedDatabase)
+	} else if err != nil {
+		l.Error("error fetching slot ID by uuid", "err", err)
+		return 0, common.NewInternalServerError(common.ErrUnexpectedDatabase, err)
+	}
+	return slotID, nil
+}
