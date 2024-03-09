00a854f7747f9134ea471958f12f81fd0b8d2d8e
ashtishad <ashefgo@gmail.com>
2024-03-10 05:22:07 +0600

    Updated database schema.To reduce confusions: * id is int which is auto incremented, and used as foreign key in other tables. * uuid will be exposed to client as id,that would be beneficial for hiding actual id's to the end users, addressing security concerns.

--- db/init/01.create-database.sql
+++ db/init/01.create-database.sql
@@ -5,14 +5,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS parking_lots
 (
     id             SERIAL PRIMARY KEY,
-    parking_lot_id UUID DEFAULT uuid_generate_v4(),
+    uuid UUID DEFAULT uuid_generate_v4(),
     name           VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS slots
 (
     id             SERIAL PRIMARY KEY,
-    slot_id        UUID    DEFAULT uuid_generate_v4(),
+    uuid        UUID    DEFAULT uuid_generate_v4(),
     parking_lot_id INTEGER NOT NULL REFERENCES parking_lots (id),
     slot_number    INTEGER NOT NULL,
     is_available   BOOLEAN DEFAULT TRUE,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS slots
 CREATE TABLE IF NOT EXISTS vehicles
 (
     id                  SERIAL PRIMARY KEY,
-    vehicle_id          UUID DEFAULT uuid_generate_v4(),
+    uuid          UUID DEFAULT uuid_generate_v4(),
     registration_number VARCHAR(255) NOT NULL,
     slot_id             INTEGER      NOT NULL REFERENCES slots (id),
     parked_at           TIMESTAMPTZ    NOT NULL,
